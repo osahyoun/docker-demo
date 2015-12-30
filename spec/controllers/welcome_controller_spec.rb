ab67df269bf08bf68cdcd458970870dee421fe88
Omar Sahyoun <osahyoun@gmail.com>
2015-12-30 15:13:54 +0000

    fix spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,7 +5,7 @@ describe WelcomeController do
   describe 'show' do
     it 'renders hello' do
       get 'show'
-      expect(response.body).to eq('hell')
+      expect(response.body).to eq('hello')
     end
   end
 
